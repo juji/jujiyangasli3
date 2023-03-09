f42f23bce45c40ddb3c3b4739b005ab7062c2553
juji <jujiyangasli@gmail.com>
2023-03-09 14:04:54 +0700

    workflow

--- public/artifact/_next/static/7TfgVpf4qQ2wUAa3JiI2D/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/21eb4eb94da27f07.css","static/chunks/pages/index-f72912cb8febdb2d.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
