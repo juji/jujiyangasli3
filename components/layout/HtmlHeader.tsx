df91b871631db3993e2f6eaa8ff061af4d98bfac
juji <jujiyangasli@gmail.com>
2023-03-17 20:06:22 +0700

    manifest

--- components/layout/HtmlHeader.tsx
+++ components/layout/HtmlHeader.tsx
@@ -1,11 +1,21 @@
 import Head from 'next/head'
+import Script from 'next/script'
 
-export default function JustHead(){
+export default function HtmlHeader(){
     return <Head>
         <title>Juji | Web Developer</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Juji is a web developer. Living in Tangerang, Indonesia. Doing what he loves best." />
         <meta property="og:image" content="https://i.ibb.co/L6sFRMH/jujilogo.png" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#000000"></meta>
+        <link rel="manifest" href="manifest.json" />
+        <Script id="show-banner" strategy="afterInteractive">
+        {`
+            if (typeof navigator.serviceWorker !== 'undefined') {
+            navigator.serviceWorker.register('sw.js')
+            }
+        `}
+        </Script>
     </Head>
 }
