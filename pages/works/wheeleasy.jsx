58604111baaf2a8ad4b71f6edbfaf7c87fb398b8
juji <jujiyangasli@gmail.com>
2023-03-17 16:13:53 +0700

    wez text

--- pages/works/wheeleasy.jsx
+++ pages/works/wheeleasy.jsx
@@ -5,7 +5,30 @@ export default function Paragon(){
 
     return <WorkDetail>
         <div>
-            <p>Yo, this is awesome.</p>
+            <blockquote>
+                WheelEasy is an access information web app for people with mobility needs, their families and friends.
+            </blockquote>
+            <br />
+            <p>Great times..!!</p>
+            <p>I meet <a 
+                href="https://www.maxburt.com/" target="_blank"
+                    rel="noopener norefferer">Max Burt</a> online, and
+                    he needed someone to continue developing WheelEasy with him.
+            </p>
+            <p>
+                We rewrite the site from scratch, added loads of stuff, and 4 years passed while we do this. Now, 
+                it's alive with users and volunteers, <i>viola</i>!!
+            </p>
+            <br />
+            <p>Technologies&nbsp;used:{' '}
+                <a href="https://nextjs.org/" rel="noopener noreferrer" target="_blank">Next.js</a>&#8288;,{' '}
+                <a href="https://v4.mui.com/" rel="noopener noreferrer" target="_blank">Material-UI</a>&#8288;,{' '}
+                <a href="https://expressjs.com/" rel="noopener noreferrer" target="_blank">ExpressJs</a>&#8288;,{' '}
+                <a href="https://stripe.com/" rel="noopener noreferrer" target="_blank">stripe</a>&#8288;,{' '}
+                <a href="https://min.io/" rel="noopener noreferrer" target="_blank">min.io</a>&#8288;,{' '}
+                <a href="https://www.algolia.com/" rel="noopener noreferrer" target="_blank">Algolia</a>&#8288;,{' '}
+                <a href="https://platform.sh//" rel="noopener noreferrer" target="_blank">platform.sh</a>&#8288;.{' '}
+            </p>
         </div>
     </WorkDetail>
 
