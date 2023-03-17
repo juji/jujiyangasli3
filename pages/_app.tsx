bf4a306acc20788d768236c24785e447f9b4f3fc
juji <jujiyangasli@gmail.com>
2023-03-17 19:32:28 +0700

    service worker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,8 @@ import Header from '@/components/layout/Header'
 import Menu from '@/components/layout/Menu'
 import Pendulum from '@/components/layout/Pendulum'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
+
 
 import type { AppProps } from 'next/app'
 
@@ -20,6 +22,13 @@ export default function App({ Component, pageProps }: AppProps) {
   },[ router.pathname ])
 
   return <>
+    <Script id="show-banner" strategy="afterInteractive">
+      {`
+        if (typeof navigator.serviceWorker !== 'undefined') {
+          navigator.serviceWorker.register('sw.js')
+        }
+      `}
+    </Script>
     <Head />
     <Header isNotHome={isNotHome} />
     <Pendulum isNotHome={isNotHome} />
