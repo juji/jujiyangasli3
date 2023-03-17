14dada0bcf45860cff14cedbb16df4c579287aad
juji <jujiyangasli@gmail.com>
2023-03-17 20:09:04 +0700

    manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ import Header from '@/components/layout/Header'
 import Menu from '@/components/layout/Menu'
 import Pendulum from '@/components/layout/Pendulum'
 import { useRouter } from 'next/router'
-
+import Script from 'next/script'
 
 import type { AppProps } from 'next/app'
 
@@ -22,6 +22,13 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return <>
     <HtmlHead />
+    <Script id="sw-register" strategy="afterInteractive">
+        {`
+            if (typeof navigator.serviceWorker !== 'undefined') {
+            navigator.serviceWorker.register('sw.js')
+            }
+        `}
+        </Script>
     <Header isNotHome={isNotHome} />
     <Pendulum isNotHome={isNotHome} />
     <Menu isNotHome={isNotHome} />
