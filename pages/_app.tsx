832967bea82dd75f6c2e3fd17a97274f1c983c52
juji <jujiyangasli@gmail.com>
2023-03-17 20:06:49 +0700

    manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,11 @@ import '@/styles/vars.css'
 import '@/styles/globals.css'
 
 import { useEffect, useState } from 'react'
-import Head from '@/components/layout/JustHead'
+import HtmlHead from '@/components/layout/HtmlHeader'
 import Header from '@/components/layout/Header'
 import Menu from '@/components/layout/Menu'
 import Pendulum from '@/components/layout/Pendulum'
 import { useRouter } from 'next/router'
-import Script from 'next/script'
 
 
 import type { AppProps } from 'next/app'
@@ -22,14 +21,7 @@ export default function App({ Component, pageProps }: AppProps) {
   },[ router.pathname ])
 
   return <>
-    <Script id="show-banner" strategy="afterInteractive">
-      {`
-        if (typeof navigator.serviceWorker !== 'undefined') {
-          navigator.serviceWorker.register('sw.js')
-        }
-      `}
-    </Script>
-    <Head />
+    <HtmlHead />
     <Header isNotHome={isNotHome} />
     <Pendulum isNotHome={isNotHome} />
     <Menu isNotHome={isNotHome} />
