1a6e85c78fe3584d90fd2e4ef00d78fa7c26c79b
juji <jujiyangasli@gmail.com>
2023-03-21 00:45:01 +0700

    use h1 for opening

--- components/HomePage/index.jsx
+++ components/HomePage/index.jsx
@@ -3,13 +3,13 @@ import styles from './home.module.scss'
 export default function Home(){
 
     return <div className={`${styles.hello}`} id="hello">
-        <p className={styles.hellobig}>Hello,</p>
-        <p className={styles.p1}>My name is <b>Tri Rahmat Gunadi</b>,</p>
-        <p className={styles.p2}>but people call me <a rel='noreferrer noopener'
+        <h1 className={styles.hellobig}>Hello,</h1>
+        <h2 className={styles.p1}>My name is <b>Tri Rahmat Gunadi</b>,</h2>
+        <h2 className={styles.p2}>but people call me <a rel='noreferrer noopener'
             target="_blank"
             href='https://github.com/juji'
-            className={styles.juji}>juji</a>.</p>
-        <p className={styles.p3}>I am a web developer.</p>
+            className={styles.juji}>juji</a>.</h2>
+        <h2 className={styles.p3}>I am a web developer.</h2>
         <br />
         <div id="smiley" className={styles.smiley}>;)</div>
     </div>
