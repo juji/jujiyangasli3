ded0b761667315b76b153fb71d19297c65d98a56
juji <jujiyangasli@gmail.com>
2023-05-14 09:22:02 +0700

    swjs update

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,11 @@
-// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
-
-const CACHE = "jujiyangasli-offline-page";
+// This is the "Offline page" service worker
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackAssets = "offline";
+const CACHE = "jujiyangasli-offline-page";
 
-if (workbox){
-  console.log('workbox is LOADED')
-  workbox.setConfig({debug: false});
-}
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "offline";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -21,20 +16,13 @@ self.addEventListener("message", (event) => {
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackAssets))
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
 if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
-
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
 
 self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
@@ -51,7 +39,7 @@ self.addEventListener('fetch', (event) => {
       } catch (error) {
 
         const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackAssets);
+        const cachedResp = await cache.match(offlineFallbackPage);
         return cachedResp;
       }
     })());
--- public/sw0.js
+++ public/sw0.js
@@ -1,11 +1,16 @@
-// This is the "Offline page" service worker
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
 const CACHE = "jujiyangasli-offline-page";
 
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline";
+const offlineFallbackAssets = "offline";
+
+if (workbox){
+  console.log('workbox is LOADED')
+  workbox.setConfig({debug: false});
+}
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -16,13 +21,20 @@ self.addEventListener("message", (event) => {
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+      .then((cache) => cache.add(offlineFallbackAssets))
   );
 });
 
 if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
 
 self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
@@ -39,7 +51,7 @@ self.addEventListener('fetch', (event) => {
       } catch (error) {
 
         const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
+        const cachedResp = await cache.match(offlineFallbackAssets);
         return cachedResp;
       }
     })());
