0690f85fb855725f60bd6f970eba8f9c3fa9533d
juji <jujiyangasli@gmail.com>
2023-05-14 14:22:52 +0700

    nojs support

--- pages/works/paragon/btz.tsx
+++ pages/works/paragon/btz.tsx
@@ -1,27 +1,10 @@
 import WorkDetail from '@/components/Works/detail'
-import { NextPageContext } from 'next';
+import ParagonContent from '@/components/Works/contents/Paragon'
 
 export default function Paragon(){
 
     return <WorkDetail btz={true}>
-        <div>
-            <p>The First website of my creation, that i like. Developed with PHP, MySql.</p>
-            <p>Using CPanel as a server management tool.</p>
-            <p>It was 2012, i guess... I don&lsquo;t remember.</p>
-            <p>
-                I had a great time creating this site. The client was great: <a target="_blank" rel="noopener noreferrer" href="https://www.tauziahotels.com/">https://www.tauziahotels.com/</a>.
-                The designer was awesome: <a href="http://visious.co/" rel="noopener noreferrer" target="_blank">http://visious.co/</a>.
-            </p>
-            <p>
-                I worked in visious at the time. The team was cute and heart warming.
-                The kind of team that you would describe more as a group of friends having fun.
-            </p>
-            <p>
-                Click the logo to see the menu and explore. I was so proud of the page transition. There was no
-                react and node.js.
-                Just plain old PHP, mysql and an Apache Webserver.
-            </p>
-        </div>
+        <ParagonContent />
     </WorkDetail>
 
 }
