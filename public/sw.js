1640a4a92e05957b9672acd91a41a92ab44b0efa
juji <jujiyangasli@gmail.com>
2023-05-14 09:28:01 +0700

    swjs update

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "jujiyangasli-offline-page";
+const CACHE = "jujiyangasli-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "offline";
