ab8fc3e05733c9f4ae20a000a908cc233f96ccd8
juji <jujiyangasli@gmail.com>
2023-09-06 00:42:41 +0700

    footer

--- components/layout/Footer/index.tsx
+++ components/layout/Footer/index.tsx
@@ -1,17 +1,23 @@
+'use client';
+
 import styled from './footer.module.scss'
 import FooterMenu from '@/components/layout/Menu/footer'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 
 export default function Footer({ className }:{ className: string }){
 
+  const pathname = usePathname()
+  const isNotHome = pathname !== '/'
+
   return <footer className={`${className} ${styled.footer}`}>
+    {isNotHome ? <FooterMenu className={styled.menu} /> : null}
     <div>
       &copy; {new Date().getFullYear()} Tri Rahmat Gunadi (<Link 
         className={styled.mailToLink}
         target="_blank" href="mailto:him@jujiyangasli.com">him@jujiyangasli.com</Link>). 
         All&nbsp;rights&nbsp;reserved.
     </div>
-    <FooterMenu className={styled.menu} />
   </footer>
 
 }
