52f81b3d5a4b795b5cb067f05353f8fe5a8f691d
juji <jujiyangasli@gmail.com>
2023-09-06 00:47:49 +0700

    footer

--- components/layout/Footer/index.tsx
+++ components/layout/Footer/index.tsx
@@ -13,10 +13,12 @@ export default function Footer({ className }:{ className: string }){
   return <footer className={`${className} ${styled.footer}`}>
     {isNotHome ? <FooterMenu className={styled.menu} /> : null}
     <div className={styled.copy}>
+      <div>
       &copy; {new Date().getFullYear()} Tri Rahmat Gunadi (<Link 
         className={styled.mailToLink}
         target="_blank" href="mailto:him@jujiyangasli.com">him@jujiyangasli.com</Link>). 
         All&nbsp;rights&nbsp;reserved.
+      </div>
     </div>
   </footer>
 
